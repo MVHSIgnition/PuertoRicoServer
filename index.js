9aefb04301fc2489de8e678d6fa51131a44033d6
Anson Bridges <bridges.anson@gmail.com>
2017-12-16 14:34:45 -0800

    my name inigo montoya

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ var io = require("socket.io")(server);
 app.use(express.static(__dirname));
 
 server.listen(process.env.PORT || 1266, function() {
-	var port = server.address().port;
-	console.log("Server running on port %s", port);
+    var port = server.address().port;
+    console.log("Server running on port %s", port);
 });
 
 io.on("connection", function(socket) {
-	
+    socket.on("info request",function(msg){
+        //process request
+        console.log("info request received!");
+    });
 });
