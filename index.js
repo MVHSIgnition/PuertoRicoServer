7953cbbeb1189193edfe1fc570804f0eeb5f31f3
Anson Bridges <bridges.anson@gmail.com>
2017-12-16 15:19:29 -0800

    code cool kids code

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var io = require("socket.io")(server);
 
 app.use(express.static(__dirname));
 
+var dir  = "./transcript.txt";
+var file = new File(dir);
+
 server.listen(process.env.PORT || 1266, function() {
     var port = server.address().port;
     console.log("Server running on port %s", port);
@@ -13,6 +16,11 @@ server.listen(process.env.PORT || 1266, function() {
 io.on("connection", function(socket) {
     socket.on("info request",function(msg){
         //process request
-        console.log("info request received!");
+        file.open("r");
+        while(!file.eof){
+            var line = file.readln();
+            if(line.includes(msg))console.log(line);
+        }
+        file.close();
     });
 });
