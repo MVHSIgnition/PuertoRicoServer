25f89efc8db11d87650a689908aa1b8cead0c824
Jony <liu.z.jonathan@gmail.com>
2017-12-16 17:09:04 -0800

    return "xd";

--- index.js
+++ index.js
@@ -15,14 +15,18 @@ server.listen(process.env.PORT || 1266, function() {
 
 io.on("connection", function(socket) {
     socket.on("info request",function(msg){
-        var jsonf;
         fs.readFile("./transcript.json","utf8",function(err,contents){
             if(err)throw err;
-            jsonf = JSON.parse(contents);
+            var jsonf = JSON.parse(contents);
+						var matches = [];
             for(var i = 0; i < jsonf.data.length; i++){
                 line = jsonf.data[i];
-                if(line.includes(msg.toUpperCase()))console.log(line);
+                if(line.includes(msg.toUpperCase())) {
+									console.log(line);
+									matches.push(line);
+								}
             }
+						socket.emit("info response", matches);
         });
     });
     socket.on("info add",function(msg){
