8be9085454a5ea32716d5c695a88e9e6b7f037a0
Jony <liu.z.jonathan@gmail.com>
2017-12-17 00:23:42 -0800

    plz wokr

--- index.js
+++ index.js
@@ -5,9 +5,27 @@ var io = require("socket.io")(server);
 var fs = require("fs");
 var request = require("request");
 
-app.use(express.static(__dirname));
+//app.use(express.static(__dirname));
 
-//var dir  = "./broadcast.txt";
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://puertoricoserver.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 server.listen(process.env.PORT || 1266, function() {
     var port = server.address().port;
