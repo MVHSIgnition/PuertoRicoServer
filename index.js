29b03b1a19e286623ef22306eca716c1143d3090
Jony <liu.z.jonathan@gmail.com>
2017-12-17 00:54:57 -0800

    werwer

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var request = require("request");
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://mvhsignition.github.io/PuertoRico/');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
